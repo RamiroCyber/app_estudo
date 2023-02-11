3559e7f52f2b2d4653d1819d326274e8bedb94ad
RamiroCyber <ramiro.dr@gmail.com>
2023-02-11 17:35:31 -0300

    tela de splash e login

---
+++ lib/splash/SplashScreen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    duration();
+    super.initState();
+  }
+
+  duration() {
+    return Timer(
+        const Duration(seconds: 3), (() => Navigator.of(context).pushReplacementNamed('/login')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.tealAccent,
+      child: Container(child: const Icon(Icons.monetization_on_outlined)),
+    );
+  }
+}
